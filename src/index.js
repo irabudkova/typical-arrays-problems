429e1842d0a0bc2aa09f19912a8d35ce342be51f
irabudkova <thehive@list.ru>
2020-09-29 21:09:21 +0300

    feat: add min, max, avr functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
+exports.min = function min(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
 
-exports.min = function min (array) {
-  return 0;
+  let min = array[0];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+    }
+  }
+
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+
+  let max = array[0];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+
+  let avr = Math.round((sum / array.length) * 100)
+  avr = avr / 100;
+
+  return avr;
 }
